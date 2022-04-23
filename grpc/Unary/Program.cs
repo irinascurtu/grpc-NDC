4d14f43d38f87ddfe2261c7752996c60ec20b079
irinascurtu <scurtuirina@gmail.com>
2022-04-23 11:26:25 +0300

    structure

---
+++ grpc/Unary/Program.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Core;
+
+Console.WriteLine("Hello, World!");
+
+//Channel channel = new Channel("127.0.0.1:5000", ChannelCredentials.Insecure);
+
+//var client = new Greeter.GreeterClient(channel);
+//var cts = new CancellationTokenSource();
+
+//HelloRequest request = new HelloRequest() { Name = "NDC" };
+
+//Console.WriteLine($"sending: {request.Name}");
+
+//var reply = client.SayHello(request, options: new CallOptions() { });
+
+//Console.WriteLine(reply.Message);
